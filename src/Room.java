20c400594e8ffca25e2b87162ce6f206198982a7
Imelda <20259344@studentmail.ul.ie>
2021-11-11 22:01:15 +0000

    Commit: Code for Room

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Room {
+    private  String roomType;
+    private int minOccupancy;
+    private int maxOccupancy;
+    private double[] rates = new double[];
+
+    public Room(String roomType, int minOccupancy, int maxOccupancy, double[] rates){
+        this.roomType = roomType;
+        this.minOccupancy = minOccupancy;
+        this.maxOccupancy = maxOccupancy;
+        this.rates = rates;
+    }
+
 }
