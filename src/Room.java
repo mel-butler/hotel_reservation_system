0e9e2d5896760f15313ab9a34be28329da8876cc
Imelda <20259344@studentmail.ul.ie>
2021-11-26 18:05:15 +0000

    Commit: removing code

--- src/Room.java
+++ src/Room.java
@@ -1,31 +1,5 @@
 import java.util.ArrayList;
 
 public class Room {
-    private  String roomType;
-    private int minOccupancy;
-    private int maxOccupancy;
-    private double[] rates = new double[10];
 
-    public Room(String roomType, int minOccupancy, int maxOccupancy, double[] rates){
-        this.roomType = roomType;
-        this.minOccupancy = minOccupancy;
-        this.maxOccupancy = maxOccupancy;
-        this.rates = rates;
-    }
-
-    public String getRoomType() {
-        return roomType;
-    }
-
-    public int getMinOccupancy() {
-        return minOccupancy;
-    }
-
-    public int getMaxOccupancy() {
-        return maxOccupancy;
-    }
-
-    public double[] getRates() {
-        return rates;
-    }
 }
